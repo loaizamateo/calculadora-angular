ff9f3abc32198e72a2c23a3f44eb77bfc58dfe50
Mateo Loaiza Rios <mateoloaiza1227@gmail.com>
2019-09-06 11:33:52 -0500

    Se agrega calculos con expr-eval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component , ViewEncapsulation } from '@angular/core';
+import { Parser } from 'expr-eval';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
+  styles: ['.error {color: red}'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'calculadora';
+
+  stringCalc = "";
+  htmlStr = ""
+  getValue(event){
+    this.stringCalc = this.stringCalc.concat(event.target.textContent.trim());
+    this.htmlStr = this.stringCalc;
+  }
+
+  result(){
+    try{
+      var parser = new Parser();
+      var expr = parser.parse( this.stringCalc );
+      this.htmlStr = expr.evaluate();
+    }
+    catch{
+      this.htmlStr = "<p class='error'>ERROR</p>"
+    }
+    
+    this.stringCalc = "";
+  }
 }
